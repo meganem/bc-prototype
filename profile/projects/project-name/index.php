c89cafb075ff51607e759320724fb7f72cc5bc51
Megan Miller <meganem@stanford.edu>
2013-09-15 17:07:13 -0700

    Adding icons, styles, and HTML for new node form modal popup

--- profile/projects/project-name/index.php
+++ profile/projects/project-name/index.php
@@ -62,20 +62,34 @@ include $headerInclude;
                     <svg id="timeline">
                     </svg>
 
-                    <div id="new-node">
-                        <form id="new-node-form">
-                            <div id="new-node-thumb">
-                                <img src="<?php print $pathPrefix; ?>img/nodethumb.jpg" alt="node thumbnail" width="60" height="60" />
+                    <div id="new-node" class="modal modal-above is-short">
+                        <div id="new-node-cancel">
+                            <a class="" href="#">
+                                <img class="icon-cancel" src="<?php print $pathPrefix; ?>img/icon-cancel.png" alt="Cancel" width="17" height="17" />
+                            </a>
+                        </div>
+                        <form id="new-node-form" class="">
+                            <div class="form-divider">
+                                <a id="new-node-type" href="#" class="dark-background">
+                                    <div class="icon-sketch">sketch</div>
+                                </a>
+                                <a id="new-node-image" href="#" class="dark-background">
+                                    <img class="icon-camera" src="<?php print $pathPrefix; ?>img/icon-camera.png" alt="Choose image" width="32" height="25" />
+                                </a>
                             </div>
+                            
                             <input id="new-node-form-title" name="node-title" type="text" placeholder="Name it" >
+                            <textarea id="new-node-form-desc" rows="5" name="node-desc" placeholder="Describe it"></textarea>
+                            <input id="new-node-form-url" name="node-url" type="text" placeholder="http://" >
+                            <input id="new-node-form-date" name="node-date" type="text" placeholder="09/15/2013" >
+                            <input id="new-node-form-tags" name="node-tags" type="text" placeholder="Tag it" >
                             <div id="new-node-form-buttons">
+                                <input id="new-node-delete" class="button red" type="button" value="delete">
                                 <input id="new-node-form-submit" class="button green" type="submit" value="done">
-                                <a id="new-node-more" class="button purple" href="#">more</a>
+                                <input id="new-node-more" class="button purple" type="button" value="more">
                             </div>
                         </form>
-                        <div id="new-node-cancel">
-                            <a class="" href="#">Cancel</a>
-                        </div>
+                        
 
                     </div>
 
