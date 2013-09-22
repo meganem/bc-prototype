71b3ea6c00b759d5fc980fd8eda90045c2125a6b
Megan Miller <meganem@stanford.edu>
2013-09-22 10:39:29 -0700

    Adding logged-in logic to footer menu, changing what is in the footer menu by default.

--- parts/footer.php
+++ parts/footer.php
@@ -1,13 +1,25 @@
             <div id="footer" class="dark">
                 <div class="container">
                     <div id="footer-menu" class="">
-                        <ul class="menu">
-                            <li><a href="<?php print $pathPrefix; ?>">Home</a></li>
-                            <li><a href="<?php print $pathPrefix; ?>help">Help</a></li>
-                            <li><a href="<?php print $pathPrefix; ?>account">Account</a></li>
-                            <li><a href="<?php print $pathPrefix; ?>terms">Terms of Use</a></li>
-                            <li><a href="<?php print $pathPrefix; ?>privacy">Privacy Policy</a></li>
-                        </ul>
+                        <?php if($loggedIn == false) { ?>
+                            <ul class="menu">
+                                <li><a href="<?php print $pathPrefix; ?>">Home</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>features">Features</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>pricing">Pricing</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>sign-up">Sign up</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>support">Support</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>terms">Terms of Use</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>privacy">Privacy Policy</a></li>
+                            </ul>
+                        <?php } elseif($loggedIn == true) { ?>
+                            <ul class="menu">
+                                <li><a href="<?php print $pathPrefix; ?>dashboard">Dashboard</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>support">Support</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>account">Account</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>terms">Terms of Use</a></li>
+                                <li><a href="<?php print $pathPrefix; ?>privacy">Privacy Policy</a></li>
+                            </ul>
+                        <?php } ?>
                     </div>
                     <p>© 2013 Megan Erin Miller</p>
                 </div>
