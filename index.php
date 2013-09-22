1e7acb830d45a425e5f3b8e4251f48af13784490
Megan Miller <meganem@stanford.edu>
2013-09-22 10:40:20 -0700

    Starting to add product home page content

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 // HOMEPAGE
 $nestLevel = 0;
+$frontPage = true;
 
 // DETERMINE PATH PREFIX
 $pathLevel = "../";
@@ -19,13 +20,43 @@ $footerInclude = $pathPrefix . "parts/footer.php";
 include $headerInclude; 
 ?>
 
-	        <div id="content" class="container">
-
-                <div class="light">
-                    <h1>Introducing Bloomcase</h1>
-                    <p class="lead">Show your creative process.</p>
+	        <div class="container light align-center">
+            	<h1>Bloomcase</h1>
+            	<p class="lead">Show your creative process.</p>
+            	<p>A new tool for creatives of all kinds to document and showcase creative process.</p>
+            	<div class="row">
+                    <div class="grid-4">
+                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" height="300" alt="" />
+                    	<p>Document your evolving ideas</p>
+                    </div>
+                    <div class="grid-4">
+                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" height="300" alt="" />
+                    	<p>Visualize your creative process</p>
+                	</div>
+                    <div class="grid-4">
+                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" height="300" alt="" />
+                    	<p>Tell the story behind your work</p>
+                    </div>
                 </div>
+	        </div>
 
+            <div class="container light border-top">
+                <div class="row">
+                	<p class="lead">The most interesting part of any project is how it was created.</p>
+                	<p>We believe that sharing the story behind your creative work... Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. </p>
+                </div>
+            </div>
+            <div class="container light border-top">
+                <div class="row">
+                	<p class="lead">Blogs, portfolios, and mindmapping tools don't provide the structure needed to communicate process.</p>
+                	<p>Bloomcase provides just enough structure to enable to you to tell your unique story behing your creative work. ... Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. </p>
+                </div>
+            </div>
+            <div class="container light border-top">
+                <div class="row">
+                	<p class="lead">A powerful way to present your work.</p>
+                	<p>Sed posuere consectetur est at lobortis. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</p>
+                </div>
             </div>
 
 
