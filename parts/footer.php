c89db9d0ddd4db8bcd5bd68c9ee72bd8ea1562ab
Megan Miller <meganem@stanford.edu>
2013-09-29 14:00:49 -0700

    Adding parsley extension in case we want it.

--- parts/footer.php
+++ parts/footer.php
@@ -30,6 +30,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script src="<?php print $pathPrefix; ?>js/vendor/d3.vs.min.js"></script>
         <script src="<?php print $pathPrefix; ?>js/vendor/parsley.js"></script>
+        <script src="<?php print $pathPrefix; ?>js/vendor/parsley.extend.js"></script>
         <script>window.jQuery || document.write('<script src="<?php print $pathPrefix; ?>js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
         <script src="<?php print $pathPrefix; ?>js/main.js"></script>
