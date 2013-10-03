c29c0661f476a4419a84ffcdcf4de336aa8ee47f
Megan Miller <meganem@stanford.edu>
2013-10-03 10:49:07 -0700

    Cleaning up Parsley form validation testing.

--- js/main.js
+++ js/main.js
@@ -45,56 +45,14 @@ $('#new-node-more').click(function(e) {
     $('#new-node').toggleClass('is-short is-long');
 });
 
-// New node form
-$('#new-node-form-submit').click(function(e) {
-    //alert("Your node is added!");
 
-});
-
-// Bind form to parsley
+// Bind form to parsley -- not sure if we need this?
 $( '#new-node-form' ).parsley();
+
 // Add a listener to the form
-
-
 $( '#new-node-form' ).parsley( 'addListener', {
-
-    /*var isValid = $( '#new-node-form' ).parsley ( 'validate' );
-    if(isValid == true) {
-        //$('#new-node-form-submit').attr("disabled", "");
-        console.log("Your form is valid!");
-    }*/
-
-    /*onFieldValidate: function() {
-        var isValid = $( '#new-node-form' ).parsley( 'isValid' );
-        if(isValid == true) {
-            //$('#new-node-form-submit').attr("disabled", "");
-            console.log("Your form is valid!");
-        }
-    }*/
-
-    /*onFieldSuccess: function(a) {
+    onFieldSuccess: function(a) {
         console.log("field success!");
-        var isValid = $( '#new-node-form' ).parsley( 'isValid' );
-        console.log(isValid);
-    }*/
-
-    /*
-    onFieldError: function(elem) {
-        console.log("field error!");
-    }*/
-
-    /*onFieldValidate: function ( elem ) {
-        if ( !$( elem ).is( ':visible' ) ) {
-            return true;
-            console.log("Your form is valid!");
-        }
-        return false;
-        console.log("Your form is NOT valid!");
-    }*/
+    }
 } );
-    
-// New node form validation
-// validate string is a date in javascript
-// use javascript to convert tags into pills when they type comma
-// validate URL in javascript, see if you can get a default value of http:// ?
-// onclick for the "done" button that calls a function, after validation, then pass to the map
+var isValid = $( '#new-node-form' ).parsley( 'isValid' );
