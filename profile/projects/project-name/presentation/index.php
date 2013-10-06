ebe7b9f0c07023e62cb27218ef0a2529701a322b
Megan Miller <meganem@stanford.edu>
2013-10-06 14:17:49 -0700

    Adding page for presentation development.

---
+++ profile/projects/project-name/presentation/index.php
@@ -0,0 +1,29 @@
+<?php 
+// PRESENTATION
+$nestLevel = 4;
+$loggedIn = true;
+
+// DETERMINE PATH PREFIX
+$pathLevel = "../";
+$pathPrefix = "";
+if ($nestLevel > 0) {
+	for ($i = 0; $i < $nestLevel; $i++) {
+		$pathPrefix = $pathPrefix . $pathLevel;
+	}
+}
+
+// SET PATHS FOR INCLUDE FILES
+$headerInclude = $pathPrefix . "parts/header.php";
+$footerInclude = $pathPrefix . "parts/footer.php";
+
+// INCLUDE HEADER
+include $headerInclude; 
+?>
+
+			
+<p>Presentation page will go here, with custom header/footer TBD.</p>
+
+<?php 
+//INCLUDE FOOTER
+include $footerInclude; 
+?>
