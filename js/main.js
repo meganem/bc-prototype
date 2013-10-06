587d2de55c0c940f518848121de4e7d87d3d0dfe
Megan Miller <meganem@stanford.edu>
2013-10-06 14:16:20 -0700

    Adding edit presentation page and header and styles for drop down menu

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ $(function () {
             $('.click-nav-2 .js ul').slideUp();
             $('.click-nav-2 .clicker').removeClass('active');
         }
+        if ($('.click-nav-3 .js ul').is(':visible')) {
+            $('.click-nav-3 .js ul').slideUp();
+            $('.click-nav-3 .clicker').removeClass('active');
+        }
         $('.click-nav-1 .js ul').slideToggle(200);
         $('.click-nav-1 .clicker').toggleClass('active');
         e.stopPropagation();
@@ -28,6 +32,10 @@ $(function () {
             $('.click-nav-1 .js ul').slideUp();
             $('.click-nav-1 .clicker').removeClass('active');
         }
+        if ($('.click-nav-3 .js ul').is(':visible')) {
+            $('.click-nav-3 .js ul').slideUp();
+            $('.click-nav-3 .clicker').removeClass('active');
+        }
         $('.click-nav-2 .js ul').slideToggle(200);
         $('.click-nav-2 .clicker').toggleClass('active');
         e.stopPropagation();
@@ -39,6 +47,32 @@ $(function () {
         }
     });
 });
+
+// Edit Presentation menu dropdown
+$(function () {
+    $('.click-nav-3 > ul').toggleClass('no-js js');
+    $('.click-nav-3 .js ul').hide();
+    $('.click-nav-3 .js').click(function(e) {
+        if ($('.click-nav-1 .js ul').is(':visible')) {
+            $('.click-nav-1 .js ul').slideUp();
+            $('.click-nav-1 .clicker').removeClass('active');
+        }
+        if ($('.click-nav-2 .js ul').is(':visible')) {
+            $('.click-nav-2 .js ul').slideUp();
+            $('.click-nav-2 .clicker').removeClass('active');
+        }
+        $('.click-nav-3 .js ul').slideToggle(200);
+        $('.click-nav-3 .clicker').toggleClass('active');
+        e.stopPropagation();
+    });
+    $(document).click(function() {
+        if ($('.click-nav-3 .js ul').is(':visible')) {
+            $('.click-nav-3 .js ul', this).slideUp();
+            $('.click-nav-3 .clicker').removeClass('active');
+        }
+    });
+});
+
 
 // New node form modal expose longer form
 $('#new-node-more').click(function(e) {
---
+++ profile/projects/project-name/edit-presentation/index.php
@@ -0,0 +1,85 @@
+<?php 
+// EDIT PRESENTATION
+$nestLevel = 4;
+$loggedIn = true;
+
+// DETERMINE PATH PREFIX
+$pathLevel = "../";
+$pathPrefix = "";
+if ($nestLevel > 0) {
+	for ($i = 0; $i < $nestLevel; $i++) {
+		$pathPrefix = $pathPrefix . $pathLevel;
+	}
+}
+
+// SET PATHS FOR INCLUDE FILES
+$headerInclude = $pathPrefix . "parts/header.php";
+$footerInclude = $pathPrefix . "parts/footer.php";
+
+// INCLUDE HEADER
+include $headerInclude; 
+?>
+
+			<div id="project-header" class="light">
+                <div class="container">
+                
+                    <div class="click-nav click-nav-2">
+                        <ul class="no-js">
+                            <li>
+                                <a href="#" class="clicker">
+                                    <img src="<?php print $pathPrefix; ?>img/projectthumb-sm.png" width="35" height="35" alt="project thumbnail" />
+                                </a>
+                                <ul id="project-actions-menu" class="menu dropdown">
+                                    <li><a href="#"><img class="icon-edit" src="<?php print $pathPrefix; ?>img/icon-edit.png" alt="" width="20" height="20" /> Edit description</a></li>
+                                    <li><a href="#"><img class="icon-share" src="<?php print $pathPrefix; ?>img/icon-share.png" alt="" width="20" height="20" /> Share</a></li>
+                                    <li><a href="#"><img class="icon-archive" src="<?php print $pathPrefix; ?>img/icon-archive.png" alt="" width="20" height="20" /> Archive</a></li>
+                                    <li><a href="#"><img class="icon-change-thumb" src="<?php print $pathPrefix; ?>img/icon-change-thumb.png" alt="" width="20" height="20" /> Change thumbnail</a></li>
+                                    <li class="separator"></li>
+                                    <li><a href="#"><img class="icon-edit-presentation" src="<?php print $pathPrefix; ?>img/icon-edit-presentation.png" alt="" width="20" height="20" /> Edit presentation</a></li>
+                                    <li><a href="#"><img class="icon-presentation" src="<?php print $pathPrefix; ?>img/icon-presentation.png" alt="" width="20" height="20" /> Launch presentation</a></li>
+                                    <li class="separator"></li>
+                                    <li><a href="#"><img class="icon-history" src="<?php print $pathPrefix; ?>img/icon-history.png" alt="" width="20" height="20" /> View history</a></li>
+                                </ul>
+                            </li>
+                       </ul>
+                    </div> <!-- End click-nav -->
+                    
+                    <h1 class="project-title">My Project</h1>
+
+                    <div id="edit-presentation-header">
+
+                        <div class="click-nav click-nav-3">
+                            <ul class="no-js">
+                                <li>
+                                    <a href="#" class="clicker">
+                                        <img src="<?php print $pathPrefix; ?>img/icon-edit-presentation.png" width="35" height="35" alt="" />
+                                    </a>
+                                    <ul id="edit-presentation-menu" class="menu dropdown">
+                                        <li><a href="#"><img class="icon-reorder" src="<?php print $pathPrefix; ?>img/icon-history.png" alt="" width="20" height="20" /> Change order</a></li>
+                                        <li><a href="#"><img class="icon-edit" src="<?php print $pathPrefix; ?>img/icon-edit.png" alt="" width="20" height="20" /> Edit styles</a></li>
+                                    </ul>
+                                </li>
+                           </ul>
+                        </div> <!-- End click-nav -->
+
+                        <div id="edit-presentation-menu-title">Edit Presentation</div>
+                        <div id="edit-presentation-done">
+                            <a href="#" class="button blue">Done</a>
+                        </div>
+                    </div> <!-- End edit-presentation-menu -->
+
+                    
+                </div> <!-- End container -->
+            </div> <!-- End project-header -->
+            
+	        <div class="light" >
+
+                <p>Map will go here</p>
+
+            </div> <!-- End map container -->
+
+
+<?php 
+//INCLUDE FOOTER
+include $footerInclude; 
+?>
