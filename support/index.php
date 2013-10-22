26010e39e83ba1d2ef0bd2311c798993cef0ecde
Megan Miller <meganem@stanford.edu>
2013-10-22 13:37:23 -0700

    adding support page

---
+++ support/index.php
@@ -0,0 +1,34 @@
+<?php 
+// SUPPORT
+$nestLevel = 1;
+
+// DETERMINE PATH PREFIX
+$pathLevel = "../";
+$pathPrefix = "";
+if ($nestLevel > 0) {
+	for ($i = 0; $i < $nestLevel; $i++) {
+		$pathPrefix = $pathPrefix . $pathLevel;
+	}
+}
+
+// SET PATHS FOR INCLUDE FILES
+$headerInclude = $pathPrefix . "parts/header.php";
+$footerInclude = $pathPrefix . "parts/footer.php";
+
+// INCLUDE HEADER
+include $headerInclude; 
+?>
+
+	        <div class="container constrained light">
+            	<h1>Support</h1>
+            	<p class="lead">coming soon.</p>
+            	
+	        </div>
+
+            
+
+
+<?php 
+//INCLUDE FOOTER
+include $footerInclude; 
+?>
