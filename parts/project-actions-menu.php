5a1911f80b646a65e9142cba66b3f61b5e816b23
Megan Miller <meganem@stanford.edu>
2013-10-22 14:05:43 -0700

    Moving project actions menu to it's own separate parts file to be included when needed.

---
+++ parts/project-actions-menu.php
@@ -0,0 +1,20 @@
+                    <div class="click-nav click-nav-2">
+                        <ul class="no-js">
+                            <li>
+                                <a href="#" class="clicker">
+                                    <img src="<?php print $pathPrefix; ?>img/projectthumb-sm.png" width="35" height="35" alt="project thumbnail" />
+                                </a>
+                                <ul id="project-actions-menu" class="menu dropdown">
+                                    <li><a href="#"><img class="icon-edit" src="<?php print $pathPrefix; ?>img/icon-edit.png" alt="" width="20" height="20" /> Edit description</a></li>
+                                    <li><a href="#"><img class="icon-share" src="<?php print $pathPrefix; ?>img/icon-share.png" alt="" width="20" height="20" /> Share</a></li>
+                                    <li><a href="#"><img class="icon-archive" src="<?php print $pathPrefix; ?>img/icon-archive.png" alt="" width="20" height="20" /> Archive</a></li>
+                                    <li><a href="#"><img class="icon-change-thumb" src="<?php print $pathPrefix; ?>img/icon-change-thumb.png" alt="" width="20" height="20" /> Change thumbnail</a></li>
+                                    <li class="separator"></li>
+                                    <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name/edit-presentation"><img class="icon-edit-presentation" src="<?php print $pathPrefix; ?>img/icon-edit-presentation.png" alt="" width="20" height="20" /> Edit presentation</a></li>
+                                    <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name/presentation"><img class="icon-presentation" src="<?php print $pathPrefix; ?>img/icon-presentation.png" alt="" width="20" height="20" /> Launch presentation</a></li>
+                                    <li class="separator"></li>
+                                    <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name/history"><img class="icon-history" src="<?php print $pathPrefix; ?>img/icon-history.png" alt="" width="20" height="20" /> View history</a></li>
+                                </ul>
+                            </li>
+                       </ul>
+                    </div> <!-- End click-nav -->
