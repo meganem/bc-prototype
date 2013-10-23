d078600cb7f95cb191cd9983aa368d3221d68a48
Megan Miller <meganem@stanford.edu>
2013-10-23 09:33:25 -0700

    Fixing reference to project actions menu on "new project"

--- profile/projects/new-project/index.php
+++ profile/projects/new-project/index.php
@@ -26,26 +26,7 @@ include $headerInclude;
 			<div id="project-header" class="light">
                 <div class="container">
                 
-                    <div class="click-nav click-nav-2">
-                        <ul class="no-js">
-                            <li>
-                                <a href="#" class="clicker">
-                                    <img src="<?php print $pathPrefix; ?>img/projectthumb-sm.png" width="35" height="35" alt="project thumbnail" />
-                                </a>
-                                <ul id="project-actions-menu" class="menu dropdown">
-                                    <li><a href="#"><img class="icon-edit" src="<?php print $pathPrefix; ?>img/icon-edit.png" alt="" width="20" height="20" /> Edit description</a></li>
-                                    <li><a href="#"><img class="icon-share" src="<?php print $pathPrefix; ?>img/icon-share.png" alt="" width="20" height="20" /> Share</a></li>
-                                    <li><a href="#"><img class="icon-archive" src="<?php print $pathPrefix; ?>img/icon-archive.png" alt="" width="20" height="20" /> Archive</a></li>
-                                    <li><a href="#"><img class="icon-change-thumb" src="<?php print $pathPrefix; ?>img/icon-change-thumb.png" alt="" width="20" height="20" /> Change thumbnail</a></li>
-                                    <li class="separator"></li>
-                                    <li><a href="#"><img class="icon-edit-presentation" src="<?php print $pathPrefix; ?>img/icon-edit-presentation.png" alt="" width="20" height="20" /> Edit presentation</a></li>
-                                    <li><a href="#"><img class="icon-presentation" src="<?php print $pathPrefix; ?>img/icon-presentation.png" alt="" width="20" height="20" /> Launch presentation</a></li>
-                                    <li class="separator"></li>
-                                    <li><a href="#"><img class="icon-history" src="<?php print $pathPrefix; ?>img/icon-history.png" alt="" width="20" height="20" /> View history</a></li>
-                                </ul>
-                            </li>
-                       </ul>
-                    </div> <!-- End click-nav -->
+                    <?php include $pathPrefix . "parts/project-actions-menu.php" ?>
                     
                     <h1 class="project-title">New Project</h1>
                     <div id="project-menu" class="">
