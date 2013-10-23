eeca3041cb0e162aafdf3230b6243900bc05953d
Megan Miller <meganem@stanford.edu>
2013-10-23 10:32:47 -0700

    Adding custom header footer includes for the presentation page

---
+++ parts/footer-presentation.php
@@ -0,0 +1,20 @@
+            <div id="footer" class="presentation dark">
+                <div class="container">
+                    <div id="footer-menu" class="">
+                        <ul class="menu">
+                            <li><a href="<?php print $pathPrefix; ?>">TBD</a></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- End wrapper -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="<?php print $pathPrefix; ?>js/vendor/d3.vs.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php print $pathPrefix; ?>js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+        <script src="<?php print $pathPrefix; ?>js/main.js"></script>
+    </body>
+</html>
---
+++ parts/header-presentation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="<?php print $pathPrefix; ?>css/normalize.min.css">
+        <link rel="stylesheet" href="<?php print $pathPrefix; ?>css/main.css">
+        <link rel="stylesheet" href="<?php print $pathPrefix; ?>css/master.css">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic,300italic,600italic,700italic' rel='stylesheet' type='text/css'>
+
+        <script src="<?php print $pathPrefix; ?>js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body class="<?php print $bodyclass; ?>">
+
+        <div id="wrapper">
+           
