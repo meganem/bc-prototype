5a95f4d1530d1228967fba8c456adcd3ef29cf03
Megan Miller <meganem@stanford.edu>
2013-10-23 10:16:21 -0700

    Changing project menu to be an abstracted part to include, and updating links to link to real pages

---
+++ parts/project-menu.php
@@ -0,0 +1,9 @@
+					<div id="project-menu" class="">
+                        <ul class="menu">
+                            <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name" data-title="Story Mode" class="active"><img class="icon-story-mode" src="<?php print $pathPrefix; ?>img/icon-story-mode.png" alt="Story Mode" width="30" height="30" /></a></li>
+                            <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name/presentation" data-title="Presentation Mode" ><img class="icon-presentation" src="<?php print $pathPrefix; ?>img/icon-presentation.png" alt="Presentation Mode" width="30" height="30" /></a></li>
+                            <li><a href="<?php print $pathPrefix; ?>profile/projects/project-name/blog" data-title="Blog Mode"><img class="icon-blog-mode" src="<?php print $pathPrefix; ?>img/icon-blog-mode.png" alt="Blog Mode" width="30" height="30" /></a></li>
+                            <!-- <li><a href="#"><img class="icon-group" src="<?php //print $pathPrefix; ?>img/icon-group.png" alt="" width="30" height="30" /> Group</a></li>
+                            <li><a href="#"><img class="icon-new" src="<?php //print $pathPrefix; ?>img/icon-new.png" alt="" width="30" height="30" /> New</a></li> -->
+                        </ul>
+                    </div>
