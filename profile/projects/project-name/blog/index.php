2e4965fa18465958ce3399ce8b025c2390acc373
Megan Miller <meganem@stanford.edu>
2013-10-23 10:19:41 -0700

    Adding blog mode page

---
+++ profile/projects/project-name/blog/index.php
@@ -0,0 +1,42 @@
+<?php 
+// BLOG
+$nestLevel = 4;
+$loggedIn = true;
+$bodyclass = "logged-in";
+
+// DETERMINE PATH PREFIX
+$pathLevel = "../";
+$pathPrefix = "";
+if ($nestLevel > 0) {
+	for ($i = 0; $i < $nestLevel; $i++) {
+		$pathPrefix = $pathPrefix . $pathLevel;
+	}
+}
+
+// SET PATHS FOR INCLUDE FILES
+$headerInclude = $pathPrefix . "parts/header.php";
+$footerInclude = $pathPrefix . "parts/footer.php";
+
+// INCLUDE HEADER
+include $headerInclude; 
+?>
+
+			<div id="project-header" class="light">
+                <div class="container">
+                
+                    <?php include $pathPrefix . "parts/project-actions-menu.php" ?>
+                    
+                    <h1 class="project-title">My Project</h1>
+                    <?php include $pathPrefix . "parts/project-menu.php" ?>
+
+                </div> <!-- End container -->
+            </div> <!-- End project-header -->
+			
+			<div class="light constrained container">
+				<p>Blog mode will go here.</p>
+			</div>
+
+<?php 
+//INCLUDE FOOTER
+include $footerInclude; 
+?>
