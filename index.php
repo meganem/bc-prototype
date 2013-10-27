6d7520aeecca387feaf640f9ea364e08d74dbd08
Megan Miller <meganem@stanford.edu>
2013-10-27 16:16:52 -0700

    Adding images to product homepage

--- index.php
+++ index.php
@@ -26,15 +26,15 @@ include $headerInclude;
             	<p>A new tool for creatives of all kinds to document and showcase creative process.</p>
             	<div class="row">
                     <div class="grid-4">
-                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" alt="" />
+                    	<img src="<?php print $pathPrefix; ?>img/home-hero1.png" width="300" alt="" />
                     	<p>Document your evolving ideas</p>
                     </div>
                     <div class="grid-4">
-                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" alt="" />
+                    	<img src="<?php print $pathPrefix; ?>img/home-hero2.png" width="300" alt="" />
                     	<p>Visualize your creative process</p>
                 	</div>
                     <div class="grid-4">
-                    	<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="300" alt="" />
+                    	<img src="<?php print $pathPrefix; ?>img/home-hero3.png" width="300" alt="" />
                     	<p>Tell the story behind your work</p>
                     </div>
                 </div>
@@ -43,18 +43,21 @@ include $headerInclude;
 
             <div class="container constrained light border-top">
                 <div class="row">
+                    <div class="float-left"><img src="<?php print $pathPrefix; ?>img/home-pitch-story.png" width="500" alt="" /></div>
                 	<p class="lead">The most interesting part of any project is how it was created.</p>
                 	<p>As creatives, we not only need to show our finished work, but the process we took to get there. Bloomcase enables you to share the story behind your creative work – the inspirations, ideas, and steps along the way.</p>
                 </div>
             </div>
             <div class="container constrained light border-top">
                 <div class="row">
+                    <div class="float-right"><img src="<?php print $pathPrefix; ?>img/home-pitch-visualize.png" width="500" alt="" /></div>
                 	<p class="lead">Blogs, portfolios, and mindmapping tools don't provide the structure needed to communicate process.</p>
                 	<p>Bloomcase provides just enough structure to enable you to tell the unique story of your creative process. Bloomcase creates a dynamic relational process map out of your materials that can show how your ideas evolved over time. To do this, we've developed a visual vocabulary for storytelling that helps you define key moments in your creative process.</p>
                 </div>
             </div>
             <div class="container constrained light border-top">
                 <div class="row">
+                    <div class="float-left"><img src="<?php print $pathPrefix; ?>img/home-pitch-present.png" width="500" alt="" /></div>
                 	<p class="lead">A powerful way to present your work.</p>
                 	<p>By visualizing the steps of your creative process, you can create a unique context for presenting your work. Bloomcase provides flexible presentation templates that allow you to curate a public presenation of your projects, in addition to letting visitors explore your process maps.</p>
                 </div>
