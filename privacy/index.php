cdad0e8949163307059d858b931cc668148b6001
Megan Miller <meganem@stanford.edu>
2013-10-27 14:49:41 -0700

    Adding privacy policy

--- privacy/index.php
+++ privacy/index.php
@@ -24,7 +24,95 @@ include $headerInclude;
 
                 <div class="light">
                     <h1>Privacy Policy</h1>
-                    <p class="lead">TBD</p>
+                    <p><b>What This Privacy Policy Covers </b></p>
+<p>
+This policy covers how Bloomcase ("Bloomcase," "we," "us," or "our") treats personal information that it collects and receives on www.bloomcase.com. We are not in the business of selling information about you: we use information shared with us for the sole purpose of running and improving our services. This notice tells you what information we collect, steps we take to protect and secure it, how we use and share information, and finally, how you can contact us with questions or concerns.</p>
+<p><b> Information We Gather </b></p>
+
+<ul>
+<li> Bloomcase collects personal information when you register with us, use Bloomcase products or services or post content on a Bloomcase web site. This information is limited to your name, email address, username, and any information you post on your profile and portfolio. <!-- Your password is stored securely using a one way hash function. --></li>
+
+<li> Bloomcase may also collect personal information if you enter into promotions or sweepstakes.</li>
+
+<li> Bloomcase may automatically receive and record information on our server logs from your browser, including your IP address, cookies, and the pages you request.</li>
+
+<li> Bloomcase uses information for the following general purposes: products and services provision, identification and authentication, services improvement, support, contact, research, and anonymous reporting.</li>
+</ul>
+
+<p><b> Billing information</b></p>
+<ul>
+<li> Should you sign up for any of Bloomcase's paid features, we require your billing information in order to process the transaction.</li>
+<li> We do not store your billing information, and this information will be discarded after your transaction has been processed.</li>
+<!-- <li> We will retain the last 4 digits of your credit card number, and credit card type, so that you may identify the card used in the future.</li>
+<li> If your subscription is auto-renewing, a recurring subscription will be created at our payment gateway.</li>
+<li> All transactions are processed over an industry standard SSL/TLS connection, with a minimum of 128-bit encryption.</li> -->
+</ul>
+
+<p><b> IP Address </b></p>
+<ul>
+<li>Bloomcase may occasionally obtain IP addresses from users depending upon how you access our web sites. IP addresses, browser and session information may be used for various purposes, including but not limited to: </li>
+<ul>
+<li>Diagnose and prevent service or technology problems reported by our users or engineers that are associated with the IP addresses controlled by a specific web company or ISP.</li>
+<li>Estimate the total number of users visiting Bloomcase's web sites from specific geographical regions.</li>
+<li>Help determine which users have access privileges to certain content, services or resources that we offer.</li>
+<li>Monitor and prevent fraud and abuse.</li>
+</ul></ul>
+
+<p><b>Cookies</b></p>
+<ul>
+<li>A cookie is a small amount of data, which often includes an anonymous unique identifier that is sent to your browser from a web site's computers and stored on your computer's hard drive.</li>
+<li>If you reject all cookies, you may not be able to use Bloomcase services or products that require you to "sign in" and you may not be able to take full advantage of all offerings.</li>
+<li>You can configure your browser to accept all cookies, reject all cookies or notify you when a cookie is set.</li>
+<!-- <li>Bloomcase uses its own cookies for a number of purposes, including to:</li>
+<ul>
+<li>Require you to re-enter your password after a certain period of time has elapsed to protect you against others accidentally accessing your account contents.</li>
+<li>Keep track of preferences you specify while you are using the Bloomcase Sites, Products and Services.</li>
+<li>Estimate and report our total audience size and traffic.</li>
+<li>Conduct research to improve the content and services provided on the Bloomcase Sites, Products and Services.</li>
+</ul> -->
+</ul>
+
+<p><b>Information Relating to Children</b></p>
+<ul>
+<li>Users under the age of 13 are prohibited from using Bloomcase.</li>
+</ul>
+<p><b>Information Use, Sharing and Disclosure</b></p>
+<ul>
+<li>Bloomcase does not share personal information we learn about you from your use of the Bloomcase web sites, products and services with others except as described in this policy.</li>
+<li>Bloomcase may use or share your personal information where it is necessary to complete a transaction, to operate or improve the Bloomcase products and services, or to do something that you have asked us to do.</li>
+<li>From time to time, Bloomcase or a partner may sponsor a promotion, sweepstake or contest on Bloomcase. You may be asked to provide personal information including name, email address or home address or to answer questions in order to participate. This information will be used strictly for the purpose of notifying and arranging delivery of promotional items to the person selected.</li>
+<li>When you register for Bloomcase, we collect user-submitted account information such as username and email address to identify users and send notifications related to the use of the service.</li>
+<li>When you post content on a Bloomcase web site, you have the ability to post content which may include personally identifiable information about yourself.</li>
+<li>If you post personal information online that is accessible to the public, you may receive unsolicited messages from other parties in return.</li>
+<li>Bloomcase may share personally identifiable information about you under the following circumstances:</li>
+<ul>
+<li>We may share your personal information to respond to subpoenas, court orders, or legal process, to establish or exercise our legal rights or, defend against legal claims and if in our judgment in such circumstances disclosure is required or appropriate.</li>
+<li>We may share your personal information if we believe it is necessary in order to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the physical safety of any person, violations of our various terms of use, or as otherwise required by law.</li>
+<li>As we continue to develop our business, we may also buy or sell all or part of our business. In such transactions, personally identifiable information you have shared with us is generally one of the business assets that will be transferred. The transferred personally identifiable information will remain subject to the promises made in this privacy policy or subsequent policies to which you have consented.</li></ul></ul>
+
+<p><b>Changes to this Privacy Policy</b></p>
+<p>
+Bloomcase may revise, modify or update this policy. We will notify you about significant changes in the way we treat personal information by sending a notice to the primary email address specified in your particular Bloomcase account or by placing a prominent notice on our site.</p>
+
+<p><b>Confidentiality and Security</b></p>
+<p>No data transmissions over the Internet can be guaranteed to be 100% secure. Consequently, we cannot ensure or warrant the security of any information you transmit to us and you do so at your own risk. Once we receive your transmission, we make efforts to ensure security on our systems. </p>
+<ul>
+<li>We use physical, electronic, and procedural safeguards to protect personal information about you.</li>
+<li>Your account information is password-protected. <!-- Additionally, your account's password is stored using a one way hash algorithm. --></li>
+<li>In certain areas, Bloomcase uses industry-standard SSL-encryption to protect data transmissions. However, please note that this is not a guarantee that such information may not be accessed, disclosed, altered or destroyed by breach of such firewalls and secure server software.</li>
+</ul>
+<p>
+If Bloomcase learns of a security systems breach we may attempt to notify you electronically so that you can take appropriate protective steps. By using Bloomcase or providing personal information to us you agree that we can communicate with you electronically regarding security, privacy and administrative issues relating to your use of those sites, products and services. We may post a notice on our applicable web sites if a security breach occurs. If this happens, you will need a web browser enabling you to view the applicable web sites. We may also send an email to you at the email address you have provided to us in these circumstances. Depending on where you live, you may have a legal right to receive notice of a security breach in writing. To receive free written notice of a security breach (or to withdraw your consent from receiving electronic notice) you should notify us at <a href="mailto:hello@bloomcase.com">hello@bloomcase.com</a>.</p>
+
+<p><b>Changing your Preferences and Personal Information</b></p>
+<ul>
+<li>You can edit your account information with Bloomcase at any time.</li>
+<li>You can delete your applicable account by visiting the applicable account deletion page; however, please note that some personal information, primarily your contact information, may remain in our records to the extent necessary to protect our legal interests or document compliance with regulatory requirements.</li>
+</ul>
+
+<p><b>Contacting Bloomcase</b></p>
+<p>If you have a privacy concern regarding Bloomcase, or this policy, and if you cannot satisfactorily resolve it through the use of those sites, products or services, you can write to us by email at <a href="mailto:hello@Bloomcase.com">hello@Bloomcase.com</a>.</p>
+
                 </div>
 
             </div>
