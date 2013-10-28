879677e5eb8b8bab9518999a66d463d41f78d15e
Megan Miller <meganem@stanford.edu>
2013-10-27 21:12:11 -0700

    sign in form

--- sign-in/index.php
+++ sign-in/index.php
@@ -21,7 +21,15 @@ include $headerInclude;
 
 	        <div class="container constrained light">
             	<h1>Sign In</h1>
-            	<p class="lead">put login form here.</p>
+            	<p class="small"><em>All fields are required.</em></p>
+            	<form id="sign-in-form" class="" data-validate="parsley" novalidate>
+					<input id="sign-in-form-username" name="username" type="text" placeholder="username" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3">
+					<input id="sign-in-form-password" name="password" type="password" placeholder="password" data-trigger="keyup" data-validation-minlength="4"  data-required="true" data-minlength="6" >
+					
+					<p class="small"><input id="sign-in-form-remember" name="remember" type="checkbox" data-required="true" > Remember me</p>
+			        <input id="sign-in-form-submit" class="button green" type="submit" value="sign in">
+			    </form>
+			    <p class="small">Need an account? <a href="<?php print $pathPrefix; ?>sign-up">Sign up</a></p>
             	
 	        </div>
 
