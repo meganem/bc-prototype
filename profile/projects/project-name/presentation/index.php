14cac1ce86b5107ce959c3c083b7642da05023aa
Megan Miller <meganem@stanford.edu>
2013-11-02 11:24:26 -0700

    Adding second slide template code and styles

--- profile/projects/project-name/presentation/index.php
+++ profile/projects/project-name/presentation/index.php
@@ -21,13 +21,24 @@ $footerInclude = $pathPrefix . "parts/footer-presentation.php";
 include $headerInclude; 
 ?>
 
-	<div class="slide-template slide-template-full-image">
+	<div class="slide-template slide-template-full-image hidden">
 		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
 		<div class="slide-overlay">
 			<div class="slide-type">
 				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
 			</div>
 			<div class="slide-title">Idea will go here</div>
+		</div>
+	</div>
+
+	<div class="slide-template slide-template-half-image">
+		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
+		<div class="slide-overlay">
+			<div class="slide-type">
+				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
+			</div>
+			<div class="slide-title">Idea will go here</div>
+			<p class="lead">Short description of this slide goes here. Lorem ipsum whatsome dosome ho hum. Littleum mixum a sipsum dipsum tu tripsum mopsum plopsum.</div>
 		</div>
 	</div>
 
