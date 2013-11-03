2430ccd167967e92766f52bbdc31001452047374
Megan Miller <meganem@stanford.edu>
2013-11-02 21:29:46 -0700

    removing required from sign in form "remember me" check box

--- sign-in/index.php
+++ sign-in/index.php
@@ -26,7 +26,7 @@ include $headerInclude;
 					<input id="sign-in-form-username" name="username" type="text" placeholder="username" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3">
 					<input id="sign-in-form-password" name="password" type="password" placeholder="password" data-trigger="keyup" data-validation-minlength="4"  data-required="true" data-minlength="6" >
 					
-					<p class="small"><input id="sign-in-form-remember" name="remember" type="checkbox" data-required="true" > Remember me</p>
+					<p class="small"><input id="sign-in-form-remember" name="remember" type="checkbox" > Remember me</p>
 			        <input id="sign-in-form-submit" class="button green" type="submit" value="sign in">
 			    </form>
 			    <p class="small">Need an account? <a href="<?php print $pathPrefix; ?>sign-up">Sign up</a></p>
