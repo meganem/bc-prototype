a150730669048ade94ad579b397093cbbd0eed47
Megan Miller <meganem@stanford.edu>
2013-11-05 20:44:59 -0800

    content for projects page

--- profile/projects/index.php
+++ profile/projects/index.php
@@ -2,7 +2,7 @@
 // MY PROJECTS
 $nestLevel = 2;
 $loggedIn = true;
-$bodyclass = "logged-in";
+$bodyclass = "logged-in projects";
 
 // DETERMINE PATH PREFIX
 $pathLevel = "../";
@@ -25,15 +25,44 @@ include $headerInclude;
 
                 <h1>My Projects</h1>
                  <div class="row">
-                    <div class="span8">
+                    <div class="span8 well">
+                    	<h2>My Projects</h2>
+                    	<div id="projects-search">
+                    		<form>
+                    			<input class="search" type="text" placeholder="Search projects" />
+                    			<input class="search-submit" type="submit" value="search" />
+                    		</form>
+                    	</div>
+
                     	<div class="row compact divider">
                     		<div class="float-left">
             					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
             				</div>
-            				<h3 class="project-title">Solar home</h3>
+            				<h3 class="project-title">Solar home <span class="featured">Featured</span></h3>
             				<p class="project-summary small">The final project for my Sustainable Architecture class where I was asked to design a custom, modular, solar home.</p>
+            				<p><a href="#" class="tag">architecture</a> <a href="#" class="tag">modeling</a> <a href="#" class="tag">CAD</a> <a href="#" class="tag">sustainability</a></p>
             				<p class="date small">Last updated October 27, 2013 at 5:37pm</p>
-            				<p><a href="#" class="button blue">Edit Project Details</a> <a href="#" class="button green">Open Project</a> <a href="#" class="featured">Featured</a></p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a></p>
+            			</div>
+            			<div class="row compact divider">
+                    		<div class="float-left">
+            					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
+            				</div>
+            				<h3 class="project-title">Archview Apartments <span class="featured">Featured</span></h3>
+            				<p class="project-summary small">A study of energy-efficient modern living, Archview Apartments captures the essence of an affordable, hip lifestyle.</p>
+            				<p><a href="#" class="tag">architecture</a> <a href="#" class="tag">modeling</a> <a href="#" class="tag">sustainability</a></p>
+            				<p class="date small">Last updated August 15, 2013 at 1:10pm</p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a></p>
+            			</div>
+            			<div class="row compact divider">
+                    		<div class="float-left">
+            					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
+            				</div>
+            				<h3 class="project-title">Another project</h3>
+            				<p class="project-summary small">The final project for my Sustainable Architecture class where I was asked to design a custom, modular, solar home.</p>
+            				<p><a href="#" class="tag">architecture</a> <a href="#" class="tag">CAD</a></p>
+            				<p class="date small">Last updated October 27, 2013 at 5:37pm</p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
             			</div>
             			<div class="row compact divider">
                     		<div class="float-left">
@@ -42,7 +71,7 @@ include $headerInclude;
             				<h3 class="project-title">Archview Apartments</h3>
             				<p class="project-summary small">A study of energy-efficient modern living, Archview Apartments captures the essence of an affordable, hip lifestyle.</p>
             				<p class="date small">Last updated August 15, 2013 at 1:10pm</p>
-            				<p><a href="#" class="button blue">Edit Project Details</a> <a href="#" class="button green">Open Project</a> <a href="#" class="featured">Featured</a> </p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
             			</div>
             			<div class="row compact divider">
                     		<div class="float-left">
@@ -51,7 +80,7 @@ include $headerInclude;
             				<h3 class="project-title">Solar home</h3>
             				<p class="project-summary small">The final project for my Sustainable Architecture class where I was asked to design a custom, modular, solar home.</p>
             				<p class="date small">Last updated October 27, 2013 at 5:37pm</p>
-            				<p><a href="#" class="button blue">Edit Project Details</a> <a href="#" class="button green">Open Project</a> </p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
             			</div>
             			<div class="row compact divider">
                     		<div class="float-left">
@@ -60,12 +89,15 @@ include $headerInclude;
             				<h3 class="project-title">Archview Apartments</h3>
             				<p class="project-summary small">A study of energy-efficient modern living, Archview Apartments captures the essence of an affordable, hip lifestyle.</p>
             				<p class="date small">Last updated August 15, 2013 at 1:10pm</p>
-            				<p><a href="#" class="button blue">Edit Project Details</a> <a href="#" class="button green">Open Project</a> </p>
+            				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
             			</div>
                     </div>
 
-                     <div class="span4 well">
-                     	do we need this sidebar? Use it for new project button? or for previewing project info...
+                     <div class="span4">
+                     	<p class="lead"><a href="#" class="button purple"><img class="icon-new" src="<?php print $pathPrefix; ?>img/icon-new-white.png" alt="" width="30" height="30" /> New Project</a></p>
+
+                     	<p><strong>Tip:</strong> To feature a project, edit the project details. <a href="<?php print $pathPrefix; ?>tips" class="">See more tips</a></p>
+                     	
                     </div>
                 </div>
            
