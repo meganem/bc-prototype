e00bbb45b03216bfcc6034b2715c81626ac61794
Megan Miller <meganem@stanford.edu>
2013-11-05 20:51:56 -0800

    adding featured tag to dashboard projects list

--- dashboard/index.php
+++ dashboard/index.php
@@ -41,7 +41,7 @@ include $headerInclude;
 	                    		<div class="float-left">
                 					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
                 				</div>
-                				<h3 class="project-title">Solar home</h3>
+                				<h3 class="project-title">Solar home <span class="featured">Featured</span></h3>
                 				<p class="project-summary small">The final project for my Sustainable Architecture class where I was asked to design a custom, modular, solar home.</p>
                 				<p class="date small">Last updated October 27, 2013 at 5:37pm</p>
                 				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
@@ -50,7 +50,7 @@ include $headerInclude;
 	                    		<div class="float-left">
                 					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
                 				</div>
-                				<h3 class="project-title">Archview Apartments</h3>
+                				<h3 class="project-title">Archview Apartments <span class="featured">Featured</span></h3>
                 				<p class="project-summary small">A study of energy-efficient modern living, Archview Apartments captures the essence of an affordable, hip lifestyle.</p>
                 				<p class="date small">Last updated August 15, 2013 at 1:10pm</p>
                 				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
