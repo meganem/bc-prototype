24a4e52b5a3f5855d4c93e99bc0d39f416f5cad7
Megan Miller <meganem@stanford.edu>
2013-11-09 14:23:49 -0800

    centering and well for new project form to match sign up forms

--- profile/projects/new/index.php
+++ profile/projects/new/index.php
@@ -21,18 +21,22 @@ $footerInclude = $pathPrefix . "parts/footer.php";
 include $headerInclude; 
 ?>
 
-	        <div class="container constrained light">
+	        <div class="container constrained light align-center">
 
                 <h1>New Project</h1>
+                <div class="row">
+                    <div class="well align-left center span4">
                 
-                <form id="new-project-form" class="" data-validate="parsley" novalidate action="<?php print $pathPrefix; ?>profile/projects/new-project" method="POST">
-                    <input id="new-project-form-title" name="project-title" type="text" placeholder="Name it" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3">
-                    <textarea id="new-project-form-desc" rows="5" name="project-desc" placeholder="Describe it" data-trigger="keyup" data-notblank="true" data-validation-minlength="1"></textarea>
-                    <input id="new-project-form-tags" name="project-tags" type="text" placeholder="Tag it, comma separated" data-trigger="keyup" data-notblank="true">
-                    <p><input id="new-project-form-featured" name="featured" type="checkbox"> Featured</p>
-                    <input id="new-project-form-submit" class="button purple" type="submit" value="Create Project">
-                </form>
-                <p class="small"> <a href="<?php print $pathPrefix; ?>profile/projects">Cancel</a></p>
+                        <form id="new-project-form" class="" data-validate="parsley" novalidate action="<?php print $pathPrefix; ?>profile/projects/new-project" method="POST">
+                            <input id="new-project-form-title" name="project-title" type="text" placeholder="Name it" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3">
+                            <textarea id="new-project-form-desc" rows="5" name="project-desc" placeholder="Describe it" data-trigger="keyup" data-notblank="true" data-validation-minlength="1"></textarea>
+                            <input id="new-project-form-tags" name="project-tags" type="text" placeholder="Tag it, comma separated" data-trigger="keyup" data-notblank="true">
+                            <p><input id="new-project-form-featured" name="featured" type="checkbox"> Featured</p>
+                            <input id="new-project-form-submit" class="button purple" type="submit" value="Create Project">
+                        </form>
+                        <p class="small"> <a href="<?php print $pathPrefix; ?>profile/projects">Cancel</a></p>
+                    </div>
+                </div>
 
             </div>
 
