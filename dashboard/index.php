98ee6216fe7eca826e3991d9a4374bdc092fe0cb
Megan Miller <meganem@stanford.edu>
2013-11-09 19:31:05 -0800

    blank dashboard for new users

--- dashboard/index.php
+++ dashboard/index.php
@@ -27,30 +27,12 @@ include $headerInclude;
                 		<p><strong>Hi Laurie!</strong> We just added the ability for you to view your project's history – <a href="#">click here to check it out</a>.</p>
                 	</div> -->
                     <div class="row">
-	                    <div class="span8 well">
+	                    <div id="dashboard-projects" class="span8 well">
                     	
                     		<h2>My Projects</h2>
-                    		<div class="row compact divider">
-	                    		<div class="float-left">
-                					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
-                				</div>
-                				<h3 class="project-title">Solar home <span class="featured">Featured</span></h3>
-                				<p class="project-summary small">The final project for my Sustainable Architecture class where I was asked to design a custom, modular, solar home.</p>
-                				<p><a href="#" class="tag">architecture</a> <a href="#" class="tag">modeling</a> <a href="#" class="tag">CAD</a> <a href="#" class="tag">sustainability</a></p>
-                				<p class="date small">Last updated October 27, 2013 at 5:37pm</p>
-                				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
-                			</div>
-                			<div class="row compact divider">
-	                    		<div class="float-left">
-                					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="100" alt="" />
-                				</div>
-                				<h3 class="project-title">Archview Apartments <span class="featured">Featured</span></h3>
-                				<p class="project-summary small">A study of energy-efficient modern living, Archview Apartments captures the essence of an affordable, hip lifestyle.</p>
-                				<p><a href="#" class="tag">architecture</a> <a href="#" class="tag">modeling</a> <a href="#" class="tag">sustainability</a></p>
-                				<p class="date small">Last updated August 15, 2013 at 1:10pm</p>
-                				<p><a href="#" class="button blue">Edit</a> <a href="#" class="button green">Open</a> </p>
-                			</div>
-                			<p><a href="<?php print $pathPrefix; ?>profile/projects" class="">See all projects</a> | <a href="<?php print $pathPrefix; ?>profile/projects/new" class="">New project</a></p>
+                    		<p>Get started by making your first project!</p>
+                    		<p class="lead"><a href="<?php print $pathPrefix; ?>profile/projects/new" class="button purple"><img class="icon-new" src="<?php print $pathPrefix; ?>img/icon-new-white.png" alt="" width="30" height="30" /> New Project</a></p>
+                			
                     	
 
 	                    </div>
@@ -60,10 +42,10 @@ include $headerInclude;
 	                    		<h2>My Profile</h2>
 	                    		<div class="row divider">
 	                				<div class="">
-	                					<img src="<?php print $pathPrefix; ?>img/placeholder.png" width="336" height="165" alt="" />
+	                					<img src="<?php print $pathPrefix; ?>img/user-placeholder.png" width="336"  alt="" />
 	                				</div>
 	                				<h3>Laurie Reid</h3>
-	                				<p class="small">An aspiring architect, studying at San Francisco State University. Passionate about design, psychology, and cookies.</p>
+	                				<p class="small"><em>Edit your profile to add a description and photo.</em></p>
 	                				<p><a href="<?php print $pathPrefix; ?>profile/edit" class="button blue">Edit</a> <a href="<?php print $pathPrefix; ?>profile" class="button green">View</a> <a href="<?php print $pathPrefix; ?>account" class="button purple">My Account</a></p>
 	                    		</div>
 	                    		<h2>Public Lnks</h2>
@@ -77,22 +59,12 @@ include $headerInclude;
 	                    	</div>
 
 	                    	
-	                    	<!-- <div class="row well">
-	                    		<h2>My Activity</h2>
-	                    		<p class="notification-message"><span class="date">August 15, 2013 at 1:10pm</span> <br>
-	                    			Idea "A big idea" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
-	                    			<p class="notification-message"><span class="date">August 15, 2013 at 12:54pm</span> <br>
-	                    			Sketch "First sketches" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
-	                    			<p class="notification-message"><span class="date">August 15, 2013 at 12:42pm</span> <br>
-	                    			<a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a> created</p>
-	                    		<p><a href="<?php print $pathPrefix; ?>history" class="">See full history</a></p>
-	                    	</div> -->
 	                    	
 	                    </div>
 	                </div><!-- end row -->
 
 	                <div id="dashboard-tips" class="row">
-                		<p><strong>Tip:</strong> In the project menu, you can edit your project details, edit your presentation, and view your history. <a href="<?php print $pathPrefix; ?>tips" class="">See more tips</a></p>
+                		<p><strong>Tip:</strong> You can view your project in three different ways: story mode, presentation mode, and blog mode. <a href="<?php print $pathPrefix; ?>tips" class="">See more tips</a></p>
                 		
 	                </div>
                     
