0b69d121974b8e93371dd9bcf46d3da97aad736c
Megan Miller <meganem@stanford.edu>
2013-11-09 19:33:09 -0800

    adding edit profile page

---
+++ profile/edit/index.php
@@ -0,0 +1,35 @@
+<?php 
+// EDIT PROFILE
+$nestLevel = 2;
+$loggedIn = true;
+$bodyclass = "logged-in";
+
+// DETERMINE PATH PREFIX
+$pathLevel = "../";
+$pathPrefix = "";
+if ($nestLevel > 0) {
+	for ($i = 0; $i < $nestLevel; $i++) {
+		$pathPrefix = $pathPrefix . $pathLevel;
+	}
+}
+
+// SET PATHS FOR INCLUDE FILES
+$headerInclude = $pathPrefix . "parts/header.php";
+$footerInclude = $pathPrefix . "parts/footer.php";
+
+// INCLUDE HEADER
+include $headerInclude; 
+?>
+
+	        <div class="container constrained light">
+	        	<h1>Edit Profile</h1>
+
+               
+
+            </div>
+
+
+<?php 
+//INCLUDE FOOTER
+include $footerInclude; 
+?>
