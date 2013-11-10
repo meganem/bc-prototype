9b1859f44eb35241f85b995b820402a1aa066ba0
Megan Miller <meganem@stanford.edu>
2013-11-09 19:47:02 -0800

    adding Edit Profile page

--- profile/edit/index.php
+++ profile/edit/index.php
@@ -21,12 +21,24 @@ $footerInclude = $pathPrefix . "parts/footer.php";
 include $headerInclude; 
 ?>
 
-	        <div class="container constrained light">
-	        	<h1>Edit Profile</h1>
-
-               
-
-            </div>
+	        <div class="container constrained light align-center">
+            	<h1>Edit Profile</h1>
+            	<div class="row">
+            		<div class="well align-left center span4">
+		            	<p class="small"><em>First and last name are required.</em></p>
+		            	<form id="edit-profile-form" class="" data-validate="parsley" novalidate action="<?php print $pathPrefix; ?>profile" method="POST">
+							<input id="edit-profile-form-firstname" name="firstname" type="text" placeholder="first name" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3"  value="Laurie">
+					        <input id="edit-profile-form-lastname" name="lastname" type="text" placeholder="last name" data-trigger="change focusout keyup" data-required="true" data-validation-minlength="3" value="Reid">
+					        <textarea id="edit-profile-form-teaser" rows="5" name="teaser" placeholder="summary" data-trigger="keyup" data-notblank="true" data-validation-minlength="1"></textarea>
+                            <textarea id="edit-profile-form-bio" rows="5" name="bio" placeholder="full bio" data-trigger="keyup" data-notblank="true" data-validation-minlength="1"></textarea>
+							<input id="edit-profile-form-url" name="profile-url" type="text" placeholder="http://"  data-trigger="keyup" data-type="url" data-validation-minlength="4">
+							<p id="edit-profile-form-link" class="small"><a href="#" class="button beige"><img class="icon-new" src="<?php print $pathPrefix; ?>img/icon-new.png" alt="" width="13" height="13" /> Add another link</a></p>
+					        <input id="edit-profile-form-submit" class="button green" type="submit" value="save">
+					    </form>
+					   
+					</div>
+        		</div>
+	        </div>
 
 
 <?php 
