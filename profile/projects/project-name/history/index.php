0786b8f8688bf0ba3a8319b18205ee967e1c4f9b
Megan Miller <meganem@stanford.edu>
2013-11-16 12:16:34 -0800

    adding example project history

--- profile/projects/project-name/history/index.php
+++ profile/projects/project-name/history/index.php
@@ -21,8 +21,28 @@ $footerInclude = $pathPrefix . "parts/footer.php";
 include $headerInclude; 
 ?>
 
-			
-<p>Project history goes here.</p>
+				<div class="container constrained light">
+                  	<h1>Project History</h1>	
+                  	<p class=""><span class="date">August 15, 2013 at 2:23pm</span> <br>
+        			Sketch "Adding second flower" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+                  	<p class=""><span class="date">August 15, 2013 at 2:15pm</span> <br>
+        			Inspiration "Dahlia picture" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>	
+					<p class=""><span class="date">August 15, 2013 at 1:25pm</span> <br>
+        			Sketch "Trying color" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+					<p class=""><span class="date">August 15, 2013 at 1:17pm</span> <br>
+        			Draft "First draft outline" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+		            <p class=""><span class="date">August 15, 2013 at 1:14pm</span> <br>
+        			Inspiration "My wedding flowers" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+        			<p class=""><span class="date">August 15, 2013 at 1:12pm</span> <br>
+        			Sketch "More sketches" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+		            <p class=""><span class="date">August 15, 2013 at 1:10pm</span> <br>
+        			Idea "A big idea" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+        			<p class=""><span class="date">August 15, 2013 at 12:54pm</span> <br>
+        			Sketch "First sketches" added to <a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a></p>
+        			<p class=""><span class="date">August 15, 2013 at 12:42pm</span> <br>
+        			<a href="<?php print $pathPrefix; ?>profile/projects/project-name">My Project</a> created</p>
+
+				</div>
 
 <?php 
 //INCLUDE FOOTER
