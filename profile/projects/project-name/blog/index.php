068a6570b8852f241dbb7f3337764332dbedc2d9
Megan Miller <meganem@stanford.edu>
2013-11-16 12:38:50 -0800

    adding more details to the blog mode HTML

--- profile/projects/project-name/blog/index.php
+++ profile/projects/project-name/blog/index.php
@@ -52,6 +52,9 @@ include $headerInclude;
 				    <div class="blog-post-tags">
 				    	<a href="#" class="tag">tattoo</a>
 				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">Considering color</a> 
+				    </div>
 				    <div id="blog-post-buttons">
 				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
 				    </div>
@@ -73,6 +76,9 @@ include $headerInclude;
 				    </div>
 				    <div class="blog-post-tags">
 				    	
+				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">1st inking session</a> 
 				    </div>
 				    <div id="blog-post-buttons">
 				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
@@ -96,10 +102,115 @@ include $headerInclude;
 				    <div class="blog-post-tags">
 				    	<a href="#" class="tag">tattoo</a>
 				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">Final draft of the outline</a> 
+				    </div>
 				    <div id="blog-post-buttons">
 				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
 				    </div>
 				</div>
+
+				<div class="blog-post">
+					<div id="blog-post-image">
+				        <img class="" src="<?php print $pathPrefix; ?>img/example/panel/finished-outline.jpg" alt="" width="347" height="" />
+				    </div>
+				    <div class="blog-post-date date">
+						September 23, 2013 at 1:40pm
+				    </div>
+				    <div class="blog-post-title">
+				    	<img class="blog-post-type" src="<?php print $pathPrefix; ?>img/icon-draft-sm.png" alt="" width="20" height="20" /> 
+				    	Final draft of the outline
+				    </div>
+				    <div class="blog-post-desc">
+				        <div>Once I added the leaves, I decided I was ready to schedule my appointment. I printed it out at the exact size I wanted on my back.</div>
+				    </div>
+				    <div class="blog-post-tags">
+				    	<a href="#" class="tag">sketch</a> <a href="#" class="tag">outline</a>
+				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">Adding the second flower</a> 
+				    </div>
+				    <div id="blog-post-buttons">
+				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
+				    </div>
+				</div>
+
+				<div class="blog-post">
+					<div id="blog-post-image">
+				        <img class="" src="<?php print $pathPrefix; ?>img/example/panel/second-flower.jpg" alt="" width="347" height="" />
+				    </div>
+				    <div class="blog-post-date date">
+						September 15, 2013 at 5:33pm
+				    </div>
+				    <div class="blog-post-title">
+				    	<img class="blog-post-type" src="<?php print $pathPrefix; ?>img/icon-draft-sm.png" alt="" width="20" height="20" /> 
+				    	Adding the second flower
+				    </div>
+				    <div class="blog-post-desc">
+				        <div>I traced the flower photo I had found and retraced my first flower so that the scale and line weights would match.</div>
+				    </div>
+				    <div class="blog-post-tags">
+				    	<a href="#" class="tag">sketch</a> <a href="#" class="tag">outline</a>
+				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">A beautiful flower</a>, <a href="#" class="post-evolvedfrom">Finishing the first flower</a> 
+				    </div>
+				    <div id="blog-post-buttons">
+				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
+				    </div>
+				</div>
+
+				<div class="blog-post">
+					<div id="blog-post-image">
+				        <img class="" src="<?php print $pathPrefix; ?>img/example/panel/dahlia-photo.jpg" alt="" width="347" height="" />
+				    </div>
+				    <div class="blog-post-date date">
+						September 15, 2013 at 11:24pm
+				    </div>
+				    <div class="blog-post-title">
+				    	<img class="blog-post-type" src="<?php print $pathPrefix; ?>img/icon-inspiration-sm.png" alt="" width="20" height="20" /> 
+				    	A beautiful flower
+				    </div>
+				    <div class="blog-post-desc">
+				        <div>This photo I found of a dahlia online was the one I decided to trace for my second flower.</div>
+				    </div>
+				    <div class="blog-post-tags">
+				    	<a href="#" class="tag">photo</a> <a href="#" class="tag">flower</a>
+				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	
+				    </div>
+				    <div id="blog-post-buttons">
+				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
+				    </div>
+				</div>
+
+				<div class="blog-post">
+					<div id="blog-post-image">
+				        <img class="" src="<?php print $pathPrefix; ?>img/example/panel/finished-first-flower.jpg" alt="" width="347" height="" />
+				    </div>
+				    <div class="blog-post-date date">
+						September 14, 2013 at 7:03pm
+				    </div>
+				    <div class="blog-post-title">
+				    	<img class="blog-post-type" src="<?php print $pathPrefix; ?>img/icon-draft-sm.png" alt="" width="20" height="20" /> 
+				    	Finishing the first flower
+				    </div>
+				    <div class="blog-post-desc">
+				        <div>I finished the first flower by removing the extra graphics from the original artwork I had found, and retracing it to make it my own.</div>
+				    </div>
+				    <div class="blog-post-tags">
+				    	<a href="#" class="tag">sketch</a> <a href="#" class="tag">outline</a>
+				    </div>
+				    <div class="blog-post-evolvedfrom">
+				    	Evolved from: <a href="#" class="post-evolvedfrom">Dahlia outline artwork</a>, <a href="#" class="post-evolvedfrom">Dahlia pictures</a>, <a href="#" class="post-evolvedfrom">Sketch Idea #1</a>, <a href="#" class="post-evolvedfrom">Sketch Idea #2</a>, <a href="#" class="post-evolvedfrom">Sketch Idea #3</a>
+				    </div>
+				    <div id="blog-post-buttons">
+				        <a id="blog-post-edit" class="button blue" href="#">edit</a>
+				    </div>
+				</div>
+
+				
 
 			</div>
 
