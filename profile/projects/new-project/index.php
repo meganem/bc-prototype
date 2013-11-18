3286b69ac9053210df3447e0ce86c87aa5e6bbbd
Megan Miller <meganem@stanford.edu>
2013-11-17 16:40:33 -0800

    adding share modal to new-project page

--- profile/projects/new-project/index.php
+++ profile/projects/new-project/index.php
@@ -219,6 +219,34 @@ include $headerInclude;
 	<div class="modal-arrow"></div>
 </div>
 
+<!-- share modal -->
+<div id="share-project" class="modal align-center hidden">
+    <div class="close">
+        <a class="" href="#" onclick="closeThis('share-project');">
+            <img class="icon-close" src="<?php print $pathPrefix; ?>img/icon-close-black.png" alt="close" width="11" height="11" />
+        </a>
+    </div>
+    <p>Share this project publicly using the following link:</p>
+    <p class="small"><strong>Story mode:</strong><br>
+        <input type="text" onclick="this.select();" name="copylink" value="http://bloomcase.com/laurie-reid/projects/project-name">
+    </p>
+    <p class="small"><strong>Presentation mode:</strong><br>
+        <input type="text" onclick="this.select();" name="copylink" value="http://bloomcase.com/laurie-reid/projects/project-name/presentation">
+    </p>
+    <p class="small"><strong>Blog mode:</strong><br>
+        <input type="text" onclick="this.select();" name="copylink" value="http://bloomcase.com/laurie-reid/projects/project-name/blog">
+    </p>
+
+    <p>Or share the link to this project through social media:</p>
+    
+    <span class='st_twitter_large' displayText='Tweet'></span>
+    <span class='st_facebook_large' displayText='Facebook'></span>
+    <span class='st_pinterest_large' displayText='Pinterest'></span>
+    <span class='st_tumblr_large' displayText='Tumblr'></span>
+    <span class='st_linkedin_large' displayText='LinkedIn'></span>
+    <span class='st_email_large' displayText='Email'></span>
+</div>
+
             </div> <!-- End bloomViz -->
 
 
