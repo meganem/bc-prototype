20e9f0faf096d6bd36ec4bf8ab975ad08d22b6de
Megan Miller <meganem@stanford.edu>
2013-11-17 18:15:28 -0800

    setting variables for presentation footer adding sample slides with real data

--- profile/projects/project-name/presentation/index.php
+++ profile/projects/project-name/presentation/index.php
@@ -3,6 +3,9 @@
 $nestLevel = 4;
 $loggedIn = true;
 $bodyclass = "logged-in";
+$projectIconPath = "img/example/icon-new-tattoo.png";
+$userName = "Megan";
+$projectName = "My new tattoo";
 
 // DETERMINE PATH PREFIX
 $pathLevel = "../";
@@ -21,25 +24,22 @@ $footerInclude = $pathPrefix . "parts/footer-presentation.php";
 include $headerInclude; 
 ?>
 	<div id="presentation-controls">
-		<div class="presentation-control previous" id="presentation-control-previous">
-			<a href="#" aria-label="Previous" role="button" tabindex="0" data-slide="prev">&lsaquo;</a>
+		<div class="presentation-control previous hidden" id="presentation-control-previous">
+			<a href="#" aria-label="Previous" role="button" tabindex="0" data-slide="prev" onclick="previousSlide();">&lsaquo;</a>
 		</div>
 		<div class="presentation-control next" id="presentation-control-next">
-			<a href="#" aria-label="Next" role="button" tabindex="0" data-slide="next">&rsaquo;</a>
+			<a href="#" aria-label="Next" role="button" tabindex="0" data-slide="next" onclick="nextSlide();">&rsaquo;</a>
 		</div>
 	</div>
 
-	<div class="slide-template slide-template-full-image">
-		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
+	<div id="slide-1" class="slide-template slide-template-full-image slide-template-project-slide">
+		<img class="full-image" src="<?php print $pathPrefix; ?>img/example/huge/tattoo-finished.jpg" />
 		<div class="slide-overlay">
-			<div class="slide-type">
-				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
-			</div>
-			<div class="slide-title">Idea will go here</div>
+			<div class="slide-title">My new tattoo</div>
 		</div>
 	</div>
 
-	<div class="slide-template slide-template-half-image hidden">
+	<div id="slide-2" class="slide-template slide-template-half-image hidden">
 		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
 		<div class="slide-overlay">
 			<div class="slide-type">
