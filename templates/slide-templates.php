a2eee361e32031a85132f282762de82196b752ba
Megan Miller <meganem@stanford.edu>
2013-11-17 18:12:37 -0800

    storing slide templates in a template file

---
+++ templates/slide-templates.php
@@ -0,0 +1,33 @@
+	<div class="slide-template slide-template-full-image">
+		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
+		<div class="slide-overlay">
+			<div class="slide-type">
+				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
+			</div>
+			<div class="slide-title">Idea will go here</div>
+		</div>
+	</div>
+
+	<div class="slide-template slide-template-half-image hidden">
+		<img class="full-image" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
+		<div class="slide-overlay">
+			<div class="slide-type">
+				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
+			</div>
+			<div class="slide-title">Idea will go here</div>
+			<p class="lead">Short description of this slide goes here. Lorem ipsum whatsome dosome ho hum. Littleum mixum a sipsum dipsum tu tripsum mopsum plopsum.</p>
+		</div>
+	</div>
+
+	<div class="slide-template slide-template-half-map hidden">
+		<div class="slide-overlay">
+			<div class="slide-image-container">
+				<img class="" src="<?php print $pathPrefix; ?>img/image-big.jpg" />
+			</div>
+			<div class="slide-type">
+				<img src="<?php print $pathPrefix; ?>img/icon-idea-lg.png" width="70" height="70" alt="idea" />
+			</div>
+			<div class="slide-title">Idea will go here</div>
+			<p class="lead">Short description of this slide goes here. Lorem ipsum whatsome dosome ho hum. Littleum mixum a sipsum dipsum tu tripsum mopsum plopsum.</p>
+		</div>
+	</div>
