d7127e337289572583357e320cf2b83bc9cdd3b2
Megan Miller <meganem@stanford.edu>
2013-11-17 18:15:09 -0800

    adding updated javascript stuff setting variables for project icon, project name, and user name

--- parts/footer-presentation.php
+++ parts/footer-presentation.php
@@ -5,7 +5,7 @@
                         <ul class="no-js">
                             <li>
                                 <a href="#" class="clicker">
-                                    <img src="<?php print $pathPrefix; ?>img/projectthumb-sm.png" width="25" height="25" alt="project thumbnail" />
+                                    <img src="<?php print $pathPrefix; print $projectIconPath; ?>" width="25" height="25" alt="project thumbnail" />
                                 </a>
                                 <ul id="project-actions-menu" class="menu dropdown">
                                     <li><a href="#"><img src="<?php print $pathPrefix; ?>img/icon-idea-sm.png" alt="" width="20" height="20" /></a></li>
@@ -22,7 +22,7 @@
                        </ul>
                     </div> <!-- End click-nav -->
 
-                    <a href="<?php print $pathPrefix; ?>profile/projects/project-name">Project Name</a> by <a href="<?php print $pathPrefix; ?>profile">Laurie</a>. Powered by 
+                    <a href="<?php print $pathPrefix; ?>profile/projects/project-name"><?php print $projectName; ?></a> by <a href="<?php print $pathPrefix; ?>profile"><?php print $userName; ?></a>. Powered by 
                     <a href="#" class="logo-link">
                         <div class="logo"><img src="<?php print $pathPrefix; ?>img/bc-logo-dark.png" width="37" height="43" alt="bloomcase logo" /></div>
                         <div class="site-title">bloomcase</div>
@@ -35,10 +35,16 @@
 
         </div><!-- End wrapper -->
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script src="<?php print $pathPrefix; ?>js/vendor/d3.vs.min.js"></script>
+        <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script> -->
+        <script src="<?php print $pathPrefix; ?>js/vendor/jquery-1.10.1.min.js"></script>
+
+        <script src="<?php print $pathPrefix; ?>js/vendor/d3.v3.min.js"></script>
+        <script src="<?php print $pathPrefix; ?>js/vendor/parsley.js"></script>
+        <script src="<?php print $pathPrefix; ?>js/vendor/parsley.extend.js"></script>
         <script>window.jQuery || document.write('<script src="<?php print $pathPrefix; ?>js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
         <script src="<?php print $pathPrefix; ?>js/main.js"></script>
+
+        
     </body>
 </html>
